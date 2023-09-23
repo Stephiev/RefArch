d483360544831e2746396a14166324b94edc0566
Stephanie <5490117+Stephiev@users.noreply.github.com>
2023-09-23 13:10:34 -0700

    EF migrations

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,4 +1,11 @@
+using LibrarySystem.GraphQL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register the ApplicationDbContext service so it can be injected into resolvers.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=snolibrary.db"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");

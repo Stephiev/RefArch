200dd28c623428f449a0a382c85bcf4bb45576bb
Stephanie <5490117+Stephiev@users.noreply.github.com>
2023-09-23 13:59:27 -0700

    AddGraphQLServer, query and mutation

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,3 +1,4 @@
+using LibrarySystem.GraphQL;
 using LibrarySystem.GraphQL.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Register the ApplicationDbContext service so it can be injected into resolvers.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=snolibrary.db"));
+builder.Services.AddGraphQLServer().AddQueryType<Query>().AddMutationType<Mutation>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
 
 app.Run();
